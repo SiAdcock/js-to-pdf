1305ec8a734aed8392ccde1504ff06856d2e32a9
Simon Adcock <simonadcock@notonthehighstreet.com>
2016-02-20 16:36:57 +0000

    Print badge circles

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-require('./lib/buildExamplePDF');
+const doc = require('./lib/createDocument')();
+const writeStream = require('./lib/createWriteStream')('output/output.pdf');
+const addCoverPage = require('./lib/addCoverPage');
+const addBadgePage = require('./lib/addBadgePage');
+const badgesData = require('./data/badges.json');
+
+doc.pipe(writeStream);
+addCoverPage(doc, ['Simon Adcock', '14 Waffle Road', 'Maple Park']);
+addBadgePage(doc, badgesData);
+doc.end();
