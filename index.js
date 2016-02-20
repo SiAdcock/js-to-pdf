1525e6c3f01549443d12aef7c4e22632e59cc440
Simon Adcock <simonadcock@notonthehighstreet.com>
2016-02-20 17:40:37 +0000

    Express server

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-const doc = require('./lib/createDocument')();
-const writeStream = require('./lib/createWriteStream')('output/output.pdf');
-const addCoverInfo = require('./lib/addCoverInfo');
-const drawBadges = require('./lib/badges/drawBadges');
-const badgesData = require('./data/badges.json');
+const server = require('./lib/createServer');
 
-//set output stream
-doc.pipe(writeStream);
-
-//draw cover info on implicitly added page
-addCoverInfo(doc, ['Simon Adcock', '14 Waffle Road', 'Maple Park']);
-
-//add pages, draw badges
-drawBadges(doc, badgesData);
-
-//write PDF file
-doc.end();
+server(8080);
