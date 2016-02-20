6b7cc1dbc8aebad3989cc42796470835e3df5bbd
Simon Adcock <simonadcock@notonthehighstreet.com>
2016-02-20 16:48:51 +0000

    Refactoring

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const doc = require('./lib/createDocument')();
 const writeStream = require('./lib/createWriteStream')('output/output.pdf');
-const addCoverPage = require('./lib/addCoverPage');
-const addBadgePage = require('./lib/addBadgePage');
+const addCoverInfo = require('./lib/addCoverInfo');
+const addBadgePages = require('./lib/addBadgePages');
 const badgesData = require('./data/badges.json');
 
+//set output stream
 doc.pipe(writeStream);
-addCoverPage(doc, ['Simon Adcock', '14 Waffle Road', 'Maple Park']);
-addBadgePage(doc, badgesData);
+
+//draw cover info on implicitly added page
+addCoverInfo(doc, ['Simon Adcock', '14 Waffle Road', 'Maple Park']);
+
+//add pages for all badges
+addBadgePages(doc, badgesData);
+
+//write PDF file
 doc.end();
