bf9a3e0f99e6cd62469d300b4d8ed0f4d00fcdf3
Simon Adcock <simonadcock@notonthehighstreet.com>
2016-02-20 16:55:25 +0000

    Extract badge functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const doc = require('./lib/createDocument')();
 const writeStream = require('./lib/createWriteStream')('output/output.pdf');
 const addCoverInfo = require('./lib/addCoverInfo');
-const addBadgePages = require('./lib/addBadgePages');
+const drawBadges = require('./lib/badges/drawBadges');
 const badgesData = require('./data/badges.json');
 
 //set output stream
@@ -10,8 +10,8 @@ doc.pipe(writeStream);
 //draw cover info on implicitly added page
 addCoverInfo(doc, ['Simon Adcock', '14 Waffle Road', 'Maple Park']);
 
-//add pages for all badges
-addBadgePages(doc, badgesData);
+//add pages, draw badges
+drawBadges(doc, badgesData);
 
 //write PDF file
 doc.end();
